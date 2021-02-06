382e176d81b5a7ed843c55ab48a36ffcd5e19d3e
avtoriscenaria <fafterty@gmail.com>
2021-02-06 13:10:49 +0300

    add filters

--- src/ThreeItemsWidget/components/Modal/Modal.js
+++ src/ThreeItemsWidget/components/Modal/Modal.js
@@ -1,27 +1,51 @@
 import {createPortal} from 'react-dom'
 import React from 'react';
-import {Paper, Backdrop} from '@material-ui/core';
+import {Paper, Backdrop, Button, TextField, FormControl, InputLabel, Select, MenuItem} from '@material-ui/core';
 import t from '../../constants/translations.json';
 import { makeStyles } from '@material-ui/core/styles';
 import stylesObject from "./styles";
+import SelectedItems from "../../views/SelectedItems";
 
 const useStyles = makeStyles(stylesObject);
 
 const Modal = ({close}) => {
-    const {_paper, _backDrop, _label, _filters, _scroll_area} = useStyles()
+    const {_all_area, _paper, _backDrop, _label, _filters, _scroll_area, _buttons} = useStyles()
 
     return createPortal(
-        <Backdrop open={true} className={_backDrop} onClick={close}>
+        <div className={_all_area}>
+            <Backdrop open={true} className={_backDrop} onClick={close}/>
             <Paper elevation={3} className={_paper}>
                 <div className={_label}>{t.dialog_title}</div>
                 <div className={_filters}>
-
+                    <TextField id="outlined-basic" label={t.search} variant="outlined" />
+                    <FormControl variant="outlined" className={''}>
+                        <InputLabel id="demo-simple-select-outlined-label">{t.no_filter}</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            value={10}
+                            onChange={() => {}}
+                            label="Age"
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>{t.no_filter}</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
                 </div>
                 <div className={_scroll_area}>
 
                 </div>
+                <SelectedItems/>
+                <div className={_buttons}>
+                    <Button variant="contained" color="secondary">{t.cancel}</Button>
+                    <Button variant="contained" color="primary">{t.save}</Button>
+                </div>
             </Paper>
-        </Backdrop>,
+        </div>,
         document.getElementById('body')
     )
 }
--- src/ThreeItemsWidget/components/Modal/styles.js
+++ src/ThreeItemsWidget/components/Modal/styles.js
@@ -1,6 +1,18 @@
 const stylesObject = (theme) => ({
+    _all_area: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'fixed',
+        top: 0,
+        left: 0
+    },
     _paper: {
-        width: 600
+        width: 600,
+        padding: 20,
+        zIndex: 150,
     },
     _backDrop: {
         zIndex: 100
@@ -9,10 +21,16 @@ const stylesObject = (theme) => ({
 
     },
     _filters: {
-
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     _scroll_area: {
 
+    },
+    _buttons: {
+        display: 'flex',
+        justifyContent: 'space-between'
     }
 });
 
